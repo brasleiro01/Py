3a78b21fe221d13d7840f1dc8592a8e19b2b06cb
brasleiro01 <marcus.v.silva@live.com>
2022-01-27 09:00:01 -0300

    for / while

---
+++ 03.py
@@ -0,0 +1,49 @@
+#informar se é primo ou não for
+# a = int(input('informe o número: '))
+# div = 0
+#
+# for x in range(1, a + 1):
+#     resto = a % x
+#     print(x, resto)
+#     if resto == 0:
+#         div += div + 1
+# if div == 2:
+#     print('número {} é primo'.format(a))
+# else:
+#     print('número {} não é primo'.format(a))
+
+#estrutura de repetição em range for
+# a = int(input('entre com com o valor: '))
+# for num in range(a):
+#     div = 0
+#     for x in range(1, num + 1):
+#         resto = num % x
+#         # print(x, resto)
+#         if resto == 0:
+#             div += 1
+#     if div == 2:
+#         print(num)
+
+# nota = int(input('entre com a nota: '))
+# while nota > 10:
+#     nota = int(input('nota inválida. informe a nota correta: '))
+
+# a = int(input('Informe o primeira nota: '))
+# while a > 10:
+#     a = int(input('nota errada. Informe a nota correta: '))
+# b = int(input('Informe o segunda nota: '))
+# while b > 10:
+#     b = int(input('nota errada. Informe a nota correta: '))
+# c = int(input('Informe o terceira nota: '))
+# while c > 10:
+#     c = int(input('nota errada. Informe a nota correta: '))
+#
+# media = (a + b + c) / 4
+# print('média: {}'.format(media))
+
+
+# a = 0
+# while a <= 10:
+#     print(a)
+#     a +=1
+
