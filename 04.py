ab1bf22fa964612e9ca69d555352a023b76508bf
brasleiro01 <marcus.v.silva@live.com>
2022-01-28 11:46:40 -0300

    lista / tuple

---
+++ 04.py
@@ -0,0 +1,77 @@
+# lista = [1, 2, 3, 4]
+# lista_animal = ['cachorro', 'baleia', 'gato']
+# lista_sortida = [10, 1, 50, 6]
+# soma = 0
+# print(lista)
+# print(lista[2])
+
+# repetição com o for na lista
+# for x in lista_animal:
+    print(x)
+
+# efetuando soma dos itens da lista
+# for x in lista:
+#     print(x)
+#     soma += x
+# print(soma)
+
+# efetuando a soma dos componentes do mesmo tipo da lista
+# print(sum(lista))
+
+# maior valor da lista
+# print(max(lista))
+
+# menor valor
+# print(min(lista))
+
+###### verificando a quantidade de elementos da lista ou da tuple
+# print(len(tuple))
+# print(len(lista))
+
+###### procurando componente na lista
+# if 'gato' in lista_animal:
+#     print('existe um gato na lista')
+# else:
+#     print('não existe gato na lista')
+
+###### add componente a lista
+# if 'lobo' in lista_animal:
+#     print('existe um lobo na lista')
+# else:
+#     print('não existe lobo porem ele será incluido na lista')
+#     lista_animal.append('lobo')
+#     print(lista_animal)
+
+###### retirando o ultimo componente da lista
+# lista_animal.pop()
+# print(lista_animal)
+
+###### retirando um componente da lista selecionando a posição
+# lista_animal.pop(1)
+# print(lista_animal)
+
+###### retirando um componente da lista informando o componente
+# lista_animal.remove('lobo')
+# print(lista_animal)
+
+######## ordenando a lista
+# lista_sortida.sort()
+# print(lista_sortida)
+
+# listando de tráz pra frente
+# lista.reverse()
+# print(lista)
+
+#######tupla imutavel não pode ser alterada######
+# tuple = (1, 10, 12)
+# print(tuple)
+
+######## convertendo lista para tupla
+# tupla_animal = tuple(lista_animal)
+
+####### verificando o tipo da variavel
+# print(type(lista))
+
+###### convertendo tupple para lista
+# list_numerica = list(tuple)
+
